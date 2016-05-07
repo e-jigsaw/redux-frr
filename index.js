334bbfd1582e9d599802b4c50b481f97d7f3f0c4
jigsaw <m@jgs.me>
2016-05-07 18:42:30 +0900

    Fix export

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {filterActions} from 'redux-ignore'
 import reduce from 'reduce-reducers'
 
-export default function reducer (...args) {
+module.exports = function (...args) {
   const [type, ...reducers] = args
   return filterActions(reduce.apply(this, reducers), [type])
 }
